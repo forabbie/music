41c4345a8c4f63682c8d7c3d0b853f61be4b2e30
abbie <supermercadoabbie@gmail.com>
2025-01-03 22:18:50 +0800

    refactor: tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'media', // or 'media' or 'class'
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
-  },
-  variants: {
     extend: {}
   },
   plugins: []
