4d24fb02c730b045a6d53abbf5235770dde903c4
abbie <supermercadoabbie@gmail.com>
2025-02-09 20:04:13 +0800

    feat: vee-validate + validation/rules + sass-embedded configuration

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/css/main.css'
+import VeeValidatePlugin from './plugins/validation'
+
+import './assets/styles/main.scss'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VeeValidatePlugin)
 
 app.mount('#app')
