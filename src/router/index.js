8daa6a34bc38e909732d726497202d5457a3a860
abbie <supermercadoabbie@gmail.com>
2025-02-16 23:04:42 +0800

    refactor: routing history

--- src/router/index.js
+++ src/router/index.js
@@ -3,20 +3,23 @@ import HomeView from '../views/HomeView.vue'
 
 import { useAuthStore } from '@/stores/auth.store'
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue')
+  }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
 })
 
 router.beforeEach((to, from, next) => {
