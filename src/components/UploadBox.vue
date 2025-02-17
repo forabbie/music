c9c4d8558f1af657aa97ff57bad2b6dc5f6a88c4
abbie <supermercadoabbie@gmail.com>
2025-02-17 22:24:39 +0800

    faet: add drag state in upload box component

--- src/components/UploadBox.vue
+++ src/components/UploadBox.vue
@@ -8,7 +8,16 @@
       <!-- Upload Dropbox -->
       <div
         class="w-full px-10 py-20 rounded text-center cursor-pointer border border-dashed border-gray-400 text-gray-400 transition duration-500 hover:text-white hover:bg-green-400 hover:border-green-400 hover:border-solid"
+        :class="{ 'bg-green-400 border-green-400 border-solid': is_dragover }"
+        @drag.prevent.stop=""
+        @dragstart.prevent.stop=""
+        @dragend.prevent.stop="is_dragover = false"
+        @dragover.prevent.stop="is_dragover = true"
+        @dragenter.prevent.stop="is_dragover = true"
+        @dragleave.prevent.stop="is_dragover = false"
+        @drop.prevent.stop="upload($event)"
       >
+        >
         <h5>Drop your files here</h5>
       </div>
       <hr class="my-6" />
@@ -36,3 +45,10 @@
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const is_dragover = ref(false)
+const upload = () => {}
+</script>
